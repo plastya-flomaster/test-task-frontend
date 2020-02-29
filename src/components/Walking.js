11b7c307dcb794ad75053d88c98e9a7ddd0f4dae
PutilovDmitriy <dmitrii.98.putilov@gmail.com>
2020-02-29 23:18:08 +0500

    -

--- src/components/Walking.js
+++ src/components/Walking.js
@@ -3,7 +3,6 @@ import Tr from './Tr';
 import Th from './Th';
 import Distance from './Distance'
 import WalkingDate from "./Date";
-import Button from "./Button";
 
 function primeriDetect(id){
     if (id % 2 !== 0){
@@ -15,8 +14,7 @@ function Walking( props ) {
     return (
      <Tr bgColor={ primeriDetect(props.walking.id) }>
         <Th marginLeft="16px"><WalkingDate date={ props.walking.date }/></Th>
-        <Th><Distance distance={ props.walking.distance }/></Th>
-        <Th minWidth={"1px"} textAlign={"center"}><Button/>I</Th> 
+        <Th><Distance distance={ props.walking.distance }/></Th> 
     </Tr>
 )}
 export default Walking;
